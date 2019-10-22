b6e728f6a36b874204ecd5cf48b2bfce62ea42a8
wangzk <15613520981@163.com>
2019-10-22 17:10:25 +0800

    2019年10月22日 17:09:42 update

---
+++ src/main/java/com/ai/dataplatform/dto/LoginDto.java
@@ -0,0 +1,20 @@
+package com.ai.dataplatform.dto;
+
+import lombok.Data;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @Title: LoginDto
+ * @ProjectName: ai_demo_dataplatform
+ * @PackageName: com.ai.dataplatform.dto
+ * @Description: TODO
+ * @author: wangzk
+ * @date: 2019-10-22 09:44
+ */
+@Data
+public class LoginDto {
+    private String userName;
+    private String passWord;
+}
